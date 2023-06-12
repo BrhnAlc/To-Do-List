9f672a65279ee5a67b59183b03f4ad562c1bdc82
burhan <alacaburhan122016@gmail.com>
2023-06-12 20:55:43 +0300

    remove

--- app.js
+++ app.js
@@ -1,72 +1,31 @@
+const inputTextButton=document.querySelector(`#inputText`);
+const addToDoButton=document.querySelector(`#addToDo`);
+const clearToDoButton=document.querySelector(`#clearToDo`);
+const toDoContainer=document.querySelector(`#toDoContainer`);
 
- const inputTextButton=document.querySelector(`#inputText`);
 
- const addToDoButton=document.querySelector(`#addToDo`);
 
- const clearToDoButton=document.querySelector(`#clearToDo`);
 
-const toDoContainer=document.querySelector(`#toDoContainer`); 
 
 
 
 addToDoButton.addEventListener(`click`, function(){
+    let paragraf=document.createElement(`p`);
+    toDoContainer.appendChild(paragraf);
+    paragraf.innerHTML=inputText.value;
+paragraf.classList.add(".paragraf-styling")
+    inputText.value="";
 
-let paragraf=document.createElement("p");
-
-toDoContainer.appendChild(paragraf);
-
-paragraf.innerHTML=inputText.value;
-
-inputText.value="";
-
-
+paragraf.addEventListener(`click`, function(){
+    paragraf.style.textDecoration="line-through"
 })
 
+paragraf.addEventListener(`dblclick`, function(){
+    paragraf.removeChild(paragraf);
+})
 
+clearToDoButton.addEventListener(`click`, function(){
+   paragraf.remove();
+})
 
-
-
-
-
-
-
-
-
-
-
-
-// const addbButton=document.querySelector(`#addToDo`);
-
-// const clearButton=document.querySelector(`#clearToDo`);
-
-// const toDoContainer=document.querySelector(`#toDoContainer`);
-
-// const inputText=document.querySelector(`#inputText`)
-
-// addbButton.addEventListener(`click`, function(){
-
-//  let paragraf =document.createElement(`p`);
-//  paragraf.classList.add(`.paragraf-styling`);
-//  toDoContainer.appendChild(paragraf);
-//  paragraf.innerHTML=inputText.value;
-//  inputText.value=""
-
-
-//  clearButton.addEventListener(`click`, function(){
-//     paragraf.style.display="none"
-//  })
-
-
-
-
-
-//  paragraf.addEventListener(`click`, function(){
-//     paragraf.style.textDecoration=`line-through`;
-//  })
-
-
-//  paragraf.addEventListener(`dblclick`, function(){
-//     toDoContainer.removeChild(paragraf);
-//  })
-
-// })
+})
