e1e5d08f4c0f3fd11a1a1109f324abf6203d3dac
burhan <alacaburhan122016@gmail.com>
2023-06-12 12:04:18 +0300

    innerHTML

--- app.js
+++ app.js
@@ -3,14 +3,18 @@
 
 const addbButton=document.querySelector(`#addToDo`);
 
-const clearButton=document.querySelector(`clearToDo`);
+const clearButton=document.querySelector(`#clearToDo`);
 
+const toDoContainer=document.querySelector(`#toDoContainer`);
 
-
+const inputText=document.querySelector(`#inputText`)
 
 addbButton.addEventListener(`click`, function(){
 
- 
+ let paragraf =document.createElement(`p`);
 
+ toDoContainer.appendChild(paragraf);
+ paragraf.innerHTML=inputText.value;
+ inputText.value=""
 
 })
