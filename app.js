e7383355eb71edb636dd20264c124eb81b8895f8
burhan <alacaburhan122016@gmail.com>
2023-06-12 21:26:03 +0300

    value

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
+
+
+
+
 const inputTextButton=document.querySelector(`#inputText`);
+
+
 const addToDoButton=document.querySelector(`#addToDo`);
+
 const clearToDoButton=document.querySelector(`#clearToDo`);
+
 const toDoContainer=document.querySelector(`#toDoContainer`);
 
 
 
 
-
-
-
 addToDoButton.addEventListener(`click`, function(){
-    let paragraf=document.createElement(`p`);
+
+    let paragraf = document.createElement("p");
+
     toDoContainer.appendChild(paragraf);
+
     paragraf.innerHTML=inputText.value;
-paragraf.classList.add(".paragraf-styling")
+
+    paragraf.addEventListener(`click`, function(){
+
+        paragraf.style.textDecoration=`line-through`
+    })
+
+
     inputText.value="";
 
-paragraf.addEventListener(`click`, function(){
-    paragraf.style.textDecoration="line-through"
-})
 
-paragraf.addEventListener(`dblclick`, function(){
-    paragraf.removeChild(paragraf);
-})
+    paragraf.addEventListener(`dblclick`,function(){
 
-clearToDoButton.addEventListener(`click`, function(){
-   paragraf.remove();
-})
+        toDoContainer.removeChild(paragraf);
+    })
+
+   clearToDoButton.addEventListener(`click`,function(){
+    
+      paragraf.remove();
+
+   })
+
 
 })
