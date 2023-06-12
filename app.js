576888bc17df2fc2b5d789bd6f3645e5a8b61cca
burhan <alacaburhan122016@gmail.com>
2023-06-12 13:16:39 +0300

    innerHTML

--- app.js
+++ app.js
@@ -1,20 +1,72 @@
 
+ const inputTextButton=document.querySelector(`#inputText`);
+
+ const addToDoButton=document.querySelector(`#addToDo`);
+
+ const clearToDoButton=document.querySelector(`#clearToDo`);
+
+const toDoContainer=document.querySelector(`#toDoContainer`); 
 
 
-const addbButton=document.querySelector(`#addToDo`);
 
-const clearButton=document.querySelector(`#clearToDo`);
+addToDoButton.addEventListener(`click`, function(){
 
-const toDoContainer=document.querySelector(`#toDoContainer`);
+let paragraf=document.createElement("p");
 
-const inputText=document.querySelector(`#inputText`)
+toDoContainer.appendChild(paragraf);
 
-addbButton.addEventListener(`click`, function(){
+paragraf.innerHTML=inputText.value;
 
- let paragraf =document.createElement(`p`);
 
- toDoContainer.appendChild(paragraf);
- paragraf.innerHTML=inputText.value;
- inputText.value=""
+
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const addbButton=document.querySelector(`#addToDo`);
+
+// const clearButton=document.querySelector(`#clearToDo`);
+
+// const toDoContainer=document.querySelector(`#toDoContainer`);
+
+// const inputText=document.querySelector(`#inputText`)
+
+// addbButton.addEventListener(`click`, function(){
+
+//  let paragraf =document.createElement(`p`);
+//  paragraf.classList.add(`.paragraf-styling`);
+//  toDoContainer.appendChild(paragraf);
+//  paragraf.innerHTML=inputText.value;
+//  inputText.value=""
+
+
+//  clearButton.addEventListener(`click`, function(){
+//     paragraf.style.display="none"
+//  })
+
+
+
+
+
+//  paragraf.addEventListener(`click`, function(){
+//     paragraf.style.textDecoration=`line-through`;
+//  })
+
+
+//  paragraf.addEventListener(`dblclick`, function(){
+//     toDoContainer.removeChild(paragraf);
+//  })
+
+// })
