0d1db1532e237a80e835bd8c5a1614a0c9cbb0ec
burhan <alacaburhan122016@gmail.com>
2023-06-12 11:39:14 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+
+const addbButton=document.querySelector(`#addToDo`);
+
+const clearButton=document.querySelector(`clearToDo`);
+
+
+
+
+addbButton.addEventListener(`click`, function(){
+
+ 
+
+
+})
